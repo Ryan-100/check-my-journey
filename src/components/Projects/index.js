fef68dec59fb107c4705159103b3f6d034d9abef
ryan <kyawzinhtet6388@gmail.com>
2023-10-10 15:09:35 +0630

    second commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From volunteering to paid jobs. Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'freelance' ?
+            <ToggleButton active value="freelance" onClick={() => setToggle('freelance')}>Freelance</ToggleButton>
+            :
+            <ToggleButton value="freelance" onClick={() => setToggle('freelance')}>Freelance</ToggleButton>
+          }
+            <Divider />
+          {toggle === 'school pj' ?
+            <ToggleButton active value="school pj" onClick={() => setToggle('school pj')}>School Projects</ToggleButton>
+            :
+            <ToggleButton value="school pj" onClick={() => setToggle('school pj')}>School Projects</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'volunteer' ?
+            <ToggleButton active value="volunteer" onClick={() => setToggle('volunteer')}>Volunteering</ToggleButton>
+            :
+            <ToggleButton value="volunteer" onClick={() => setToggle('volunteer')}>Volunteering</ToggleButton>
+          }
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
